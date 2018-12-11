4b9b60b116fef8f62528e32e1c89641a0e9e447d
lxj3715 <934896316@qq.com>
2018-12-11 16:40:38 +0800

    修改2
    修改2

--- gitdemo1/src/main/java/test.java
+++ gitdemo1/src/main/java/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("2233");
+        System.out.println("2hg");
            System.out.println();
     }
 }
